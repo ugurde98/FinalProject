c435652f1659b12aa767571e82acffa56c435742
ugurde98 <ugurde98@gmail.com>
2022-11-18 17:29:17 +0300

    Entity Framework

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
 namespace ConsoleUI
@@ -7,13 +8,25 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
             
             foreach (var item in productManager.GetAll())
             {
-                Console.WriteLine(item.ProductName);
+                Console.WriteLine(item.ProductName+ ":" +item.UnitPrice);
             }
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("--------------------------------------------------");
+            foreach (var item in productManager.GetAllByCategory(2))
+            {
+                Console.WriteLine(item.ProductName + ":" + item.CategoryId);
+            }
+            Console.WriteLine("--------------------------------------------------");
+            foreach (var item in productManager.GetByUnitPrice(10,20))
+            {
+                Console.WriteLine(item.ProductName + ":" + item.UnitPrice);
+            }
+
         }
+
     }
 }
