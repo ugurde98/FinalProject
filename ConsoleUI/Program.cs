d9ef0479d1fe9abd3fec7c086b5302259774e224
ugurde98 <ugurde98@gmail.com>
2022-11-22 16:46:34 +0300

    22.11

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,11 +8,29 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            ProductTest();
+           // CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            Console.WriteLine("--------------------------------------------------");
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+            Console.WriteLine("--------------------------------------------------");
+            //Console.WriteLine(categoryManager.GetById(2));
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            
+
             foreach (var item in productManager.GetAll())
             {
-                Console.WriteLine(item.ProductName+ ":" +item.UnitPrice);
+                Console.WriteLine(item.ProductName + ":" + item.UnitPrice);
             }
 
             Console.WriteLine("--------------------------------------------------");
@@ -21,12 +39,15 @@ namespace ConsoleUI
                 Console.WriteLine(item.ProductName + ":" + item.CategoryId);
             }
             Console.WriteLine("--------------------------------------------------");
-            foreach (var item in productManager.GetByUnitPrice(10,20))
+            foreach (var item in productManager.GetByUnitPrice(10, 20))
             {
                 Console.WriteLine(item.ProductName + ":" + item.UnitPrice);
             }
-
+            Console.WriteLine("--------------------------------------------------");
+            foreach (var item in productManager.GetProductDetails())
+            {
+                Console.WriteLine(item.ProductName+"/"+item.CategoryName);
+            }
         }
-
     }
 }
